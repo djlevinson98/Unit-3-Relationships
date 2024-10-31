8526c6f0fe43c46278759bf2704338b957f9ae55
djlevinson98 <140760896+djlevinson98@users.noreply.github.com>
2024-10-31 12:42:03 -0400

    Update Kid.java

--- Kid.java
+++ Kid.java
@@ -1,1 +1,13 @@
+public class Kid{
 
+    private Parent parent;
+
+    public Kid(Parent parent){
+        this.parent = parent;
+    }
+
+
+    public Parent getParent(){
+        return parent;
+    }
+}
