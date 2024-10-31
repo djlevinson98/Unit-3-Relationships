8e7b520f96d0ed477296dafc2fefea25b3dd5fee
djlevinson98 <140760896+djlevinson98@users.noreply.github.com>
2024-10-31 12:41:36 -0400

    Update Parent.java

--- Parent.java
+++ Parent.java
@@ -1,1 +1,21 @@
+public class Parent{
+    
+    private String parentName;
+
+    public Parent(String name){
+        parentName = name;
+    }
+
+    public String getParentName(){
+        return parentName;
+    }
+
+    public boolean equals(Parent other){
+        if (this.getParentName().equals(other.getParentName()))
+            {return true;}
+        else
+            {return false;}
+
+    }
+}
 
